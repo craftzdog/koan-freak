ee47e3a4bfcbb00018b256cb13b403556515d840
Takuya Matsuyama <nora@odoruinu.net>
2015-07-23 23:44:43 +0900

    bump

--- app/consts.js
+++ app/consts.js
@@ -2,7 +2,7 @@ var consts = {
   config_fn: __dirname + '/../config/app.json',
   tmp_dir: '/tmp',
   appId: 'webService',
-  sessionSecretKeys: ['some secret pukapuka'],
+  sessionSecretKeys: ['some secret key'],
   accessLogFileName: __dirname + '/../log/access.log'
 };
 
